a565315b422649e09333b47839f8237972ccca75
Anton Dolzhenko <dolzhenko.tony@gmail.com>
2021-05-23 14:08:08 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "QuiteGoodNetworking",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v11),
+        .watchOS(.v7)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "QuiteGoodNetworking",
+            targets: ["QuiteGoodNetworking"]),
+    ],
+    dependencies: [.package(url: "https://github.com/Alamofire/Alamofire.git",
+                            from: "5.2.2")],
+    targets: [.target(name: "QuiteGoodNetworking",
+                      dependencies: ["Alamofire"],
+                      path: "QuiteGoodNetworking/Classes")]
+)
