df66e40a10ff7100e1c928ad2ca409be1ae38b24
Anton Dolzhenko <dolzhenko.tony@gmail.com>
2021-09-12 16:53:22 +0300

    remove WhisperNetworking

--- WhisperNetworking/EndpointRequests/MessagingEndpointRequest.swift
+++
@@ -1,9 +0,0 @@
-class MessagingEndpointRequest: EndpointRequest {
-    
-    override var baseURLString: String { get { return "https://whisper.api.tigertext.me" } }
-    
-    override func headers() -> [String : String] {
-        return MessagingEndpointRequestHeaders.headers()
-    }
-    
-}
